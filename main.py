ac632f15f077558d6c0c191720845611c23c8785
Blumbahs <gunars.blumbahs@accenture.com>
2020-01-10 17:02:00 +0200

    m2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify, request
 
 
 app = Flask('app')
@@ -6,11 +6,28 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return render_template('index.html')
+  return render_template('chats.html')
 
 @app.route('/health')
 def health_check():
   return "OK"
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open('chats.txt','r', encoding="UTF-8" ) as f :
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({'chats':chata_rindas})
+
+@app.route('/chats/suuti', methods = ['POST'])
+def suuti_zinu():
+  dati = request.json
+  print(dati)
+  with open("chats.txt", "a", newline="") as f:
+    f.write(dati["chats"]+"\n")
+
+  return ielasit_chatu()
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
--- static/chats.js
+++ static/chats.js
@@ -1,1 +1,68 @@
-console.log("chats ir dzivs");
+const ATJAUNOT = 11000;
+
+async function lasiChatu(){
+  const atbilde = await fetch('/chats/lasi');
+  const datuObjekts = await atbilde.json();
+  raadiChataRindas(datuObjekts);
+  await new Promise(resolve => setTimeout(resolve, ATJAUNOT));
+  await lasiChatu();
+
+};
+
+
+async function suutiZinju(){
+    let zinjasElements = document.getElementById('zinja');
+    let zinja = zinjasElements.value;
+    
+    const atbilde = await fetch('/chats/suuti', {
+        method : 'POST',
+        headers: {
+        'Content-Type' : 'application/json'
+
+        },
+        body: JSON.stringify({"chats":zinja})
+
+    });
+    const datuObjekts = await atbilde.json();
+    raadiChataRindas(datuObjekts);
+};
+
+function raadiChatuVienkarsi(data){
+
+ const jaunaRinda = "</br>";
+ let chats = "";
+ 
+    for (let rinda of data['chats']){
+        chats += rinda + jaunaRinda;
+    }
+
+ document.getElementById("chats").innerHTML = chats;
+
+};
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  };
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  };
